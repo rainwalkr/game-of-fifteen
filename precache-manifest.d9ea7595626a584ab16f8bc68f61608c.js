a07444630b69df4836302dc6c53c496eba00edfc
Amith Francis <agusamith@gmail.com>
2018-12-29 22:23:58 +0530

    Updates

---
+++ precache-manifest.d9ea7595626a584ab16f8bc68f61608c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "100cc8ba2c7c762bd931",
+    "url": "/game-of-fifteen/static/js/runtime~main.100cc8ba.js"
+  },
+  {
+    "revision": "985da5d2d4dfbdeeb1cb",
+    "url": "/game-of-fifteen/static/js/main.985da5d2.chunk.js"
+  },
+  {
+    "revision": "5fa54fa9afb7547da21c",
+    "url": "/game-of-fifteen/static/js/1.5fa54fa9.chunk.js"
+  },
+  {
+    "revision": "985da5d2d4dfbdeeb1cb",
+    "url": "/game-of-fifteen/static/css/main.1888347e.chunk.css"
+  },
+  {
+    "revision": "9eeadcd699ed6f08916fd24207759e63",
+    "url": "/game-of-fifteen/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55723cc29d0dbfda01f45253053518bb.js"
+  "/game-of-fifteen/precache-manifest.d9ea7595626a584ab16f8bc68f61608c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/game-of-fifteen/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
